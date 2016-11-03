6ad54bfd130d728e31fc1f68f647519930cfba91
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2016-11-03 19:19:54 -0200

    add --all like parameter

--- installer.rb
+++ installer.rb
@@ -5,8 +5,16 @@ require_relative 'node'
 require_relative 'ruby'
 require_relative 'git'
 
+def all
+  Vim.install_vundle
+  Node.install
+  Ruby.install_rvm
+  Git.download_files
+end
+
 ARGV.each do |arg|
   case arg
+    when '--all' then all
     when '--vim'  then Vim.install_vundle
     when '--node' then Node.install
     when '--ruby' then Ruby.install_rvm
